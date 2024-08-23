6cc50e0fb06a11a649ae82920e27f3d41d1bf85f
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-08-23 11:03:00 -0500

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ReactNode } from "react"
+import { GoogleAnalytics } from "@next/third-parties/google"
 import Footer from "@/components/footer"
 import "./globals.css"
 
@@ -8,13 +9,14 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Lil Baghdad Iraqi Cuisine",
-  description: "Lil Baghdad food truck online menu for authentic Iraqi cuisine"
+  description: "Iraqi made, Texas inspired"
 };
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleAnalytics gaId="G-JDJYK3LM1M" />
         {children}
         <Footer />
       </body>
